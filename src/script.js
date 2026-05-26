623de41e8ed50954187dd36a0f72be89305e522b
xldrm <tema.2001234606@gmail.com>
2026-05-26 11:28:00 +0300

    Удаление конфига функций для перехода на чистую статику

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function generatePassword() {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < 16; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    document.getElementById("password").value = password;
+}
